8c0ec498525488cd831a3a47f7267b809e044f1b
chicodevbr <chicodevbr@gmail.com>
2022-10-22 21:28:00 -0300

    Initial Commit

---
+++ iConvertApp/iConvertAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConvertAppApp.swift
+//  iConvertApp
+//
+//  Created by fjunior on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct iConvertAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
